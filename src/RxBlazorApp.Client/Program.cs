6e674d3edb7a3d081617bd3b4c03469fa4b39384
Artem Serednitsky <vionoche@gmail.com>
2023-08-01 23:59:28 +0300

    Add project

---
+++ src/RxBlazorApp.Client/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using RxBlazorApp.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
