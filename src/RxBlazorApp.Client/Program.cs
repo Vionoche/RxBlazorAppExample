f5dd79f1171a306a0be1afade9cc016fd5a54d7a
Artem Serednitsky <vionoche@gmail.com>
2023-08-03 23:17:55 +0300

    Add MudBlazor for the MainLayout and the Counter page

--- src/RxBlazorApp.Client/Program.cs
+++ src/RxBlazorApp.Client/Program.cs
@@ -3,12 +3,15 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 using RxBlazorApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddMudServices()
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
